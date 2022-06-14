86d9e3ed1039fc44d16b1f8b9e52b43d009f8c63
Sergey Batukov <sashachepuha@gmail.com>
2022-06-14 20:01:29 +0300

    test

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { Application } from "pixi.js";
+import { Application, Loader, Texture, AnimatedSprite } from "pixi.js";
 
 const app = new Application({
   width: innerWidth,
@@ -6,3 +6,23 @@ const app = new Application({
 });
 
 document.body.appendChild(app.view);
+
+const loader = Loader.shared;
+
+loader.add("./assets/spritesheet.json").load(() => {
+  const textures = [];
+  for (let i = 0; i <= 7; i++) {
+    const texture = Texture.from(`server${i}.png`);
+    textures.push(texture);
+  }
+  for (let i = 7; i > 0; i--) {
+    const texture = Texture.from(`server${i}.png`);
+    textures.push(texture);
+  }
+  const sprite = new AnimatedSprite(textures);
+  sprite.play();
+  sprite.position.set(300);
+  sprite.animationSpeed = 0.2;
+  app.stage.addChild(sprite);
+});
+
