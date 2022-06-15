059e354568d8e068942754b5980b3b057f142a7a
Sergey Batukov <sashachepuha@gmail.com>
2022-06-15 09:52:08 +0300

    add app class

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,84 @@
-import { Application, Loader, Texture, AnimatedSprite } from "pixi.js";
+import { Dict } from "@pixi/utils";
+import {
+  Application,
+  Loader,
+  Texture,
+  AnimatedSprite,
+  IApplicationOptions,
+  Sprite,
+  LoaderResource,
+} from "pixi.js";
 
-const app = new Application({
-  width: innerWidth,
-  height: innerHeight,
-});
-
-document.body.appendChild(app.view);
-
-const loader = Loader.shared;
-
-loader.add("./assets/spritesheet.json").load(() => {
-  const textures = [];
-  for (let i = 0; i <= 7; i++) {
-    const texture = Texture.from(`server${i}.png`);
-    textures.push(texture);
+declare global {
+  interface Window {
+    muup: App;
   }
-  for (let i = 7; i > 0; i--) {
-    const texture = Texture.from(`server${i}.png`);
-    textures.push(texture);
+}
+
+// const app = new Application({
+//   width: innerWidth,
+//   height: innerHeight,
+// });
+
+// document.body.appendChild(app.view);
+
+// const loader = Loader.shared;
+
+// loader.add("./assets/spritesheet.json").load(() => {
+//   const textures = [];
+//   for (let i = 0; i <= 7; i++) {
+//     const texture = Texture.from(`server${i}.png`);
+//     textures.push(texture);
+//   }
+//   for (let i = 7; i > 0; i--) {
+//     const texture = Texture.from(`server${i}.png`);
+//     textures.push(texture);
+//   }
+//   const sprite = new AnimatedSprite(textures);
+//   sprite.play();
+//   sprite.position.set(300);
+//   sprite.animationSpeed = 0.2;
+//   app.stage.addChild(sprite);
+// });
+
+export class App extends Application {
+  loader: Loader;
+  constructor(selector: string, options: IApplicationOptions) {
+    super(options);
+    const container = document.querySelector(selector);
+    if (!container) document.body.appendChild(this.view);
+    else container.appendChild(this.view);
+
+    this.loader = Loader.shared;
+    this.load();
   }
-  const sprite = new AnimatedSprite(textures);
-  sprite.play();
-  sprite.position.set(300);
-  sprite.animationSpeed = 0.2;
-  app.stage.addChild(sprite);
-});
+
+  load() {
+    this.loader.add("./assets/spritesheet.json").load((l, r) => {
+      this.setup();
+    });
+  }
+  setup() {
+    const textures = [];
+    for (let i = 0; i <= 7; i++) {
+      const texture = Texture.from(`server${i}.png`);
+      textures.push(texture);
+    }
+    for (let i = 7; i > 0; i--) {
+      const texture = Texture.from(`server${i}.png`);
+      textures.push(texture);
+    }
+    const sprite = new AnimatedSprite(textures);
+    sprite.play();
+    sprite.animationSpeed = 0.2;
+    this.stage.addChild(sprite);
+  }
+}
+
+export function create(selector: string, options: IApplicationOptions) {
+  window.muup = new App(selector, options);
+  return window.muup;
+}
+
+const muup = create("#muup", {});
 
---
+++ src/services/component.service.ts
@@ -0,0 +1,6 @@
+import { Application, Loader, Texture, AnimatedSprite } from "pixi.js";
+
+export class Component {
+  sprite: AnimatedSprite | null = null;
+  constructor() {}
+}
