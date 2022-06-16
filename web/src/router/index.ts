d93fddcde542125d7d691de9c676cbee9e23b35c
Sergey Batukov <sashachepuha@gmail.com>
2022-06-16 12:01:43 +0300

    update

--- web/src/router/index.ts
+++ web/src/router/index.ts
@@ -4,17 +4,8 @@ import Home from "../views/Home.vue";
 const routes: Array<RouteRecordRaw> = [
   {
     path: "/",
-    name: "Home",
-    component: Home,
-  },
-  {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+    name: "Constructor",
+    component: () => import("../views/constructor/View.vue"),
   },
 ];
 
@@ -24,3 +15,4 @@ const router = createRouter({
 });
 
 export default router;
+
---
+++ web/src/views/constructor/View.vue
@@ -0,0 +1,20 @@
+<template lang="pug">
+.constructor
+  .block
+  .block
+  .block
+</template>
+
+<script lang="ts" setup></script>
+
+<style lang="sass">
+.constructor
+  display: grid
+  grid-template-columns: 40px 300px 1fr
+  grid-template-rows: 1fr
+  width: 100%
+  height: 100%
+.block
+  border: var(--border)
+  background: var(--bg)
+</style>
