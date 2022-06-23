9e6e7a71e06a57d521f5f25ce1b0a1fdf646a0b6
Sergey Batukov <sashachepuha@gmail.com>
2022-06-23 15:13:53 +0300

    update

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,10 +1,42 @@
 <template lang="pug">
-router-view
+//- router-view
+#ois
 </template>
 
-<script lang="ts">
-import { RIS } from "../../lib/";
-console.log(RIS);
+<script setup lang="ts">
+import { onMounted } from "vue";
+import * as PIXI from "pixi.js";
+import { create } from "../../lib/";
+import { config } from "./config.ts";
+onMounted(() => {
+  create("#ois", {
+    width: innerWidth,
+    height: innerHeight,
+  }).load(
+    config,
+    (ois) => {
+      setInterval(() => {
+        if (Math.random() > 0.5) {
+          ois.refs["server #1"].color = "#8fff00";
+          ois.refs["plane #1"].color = "#8fff00";
+          ois.refs["line #1"].color = "#8fff00";
+          // ois.refs["server #3"].select();
+          ois.refs["text #1"].color = "#8fff00";
+          ois.refs["text #1"].text = "UP";
+        } else {
+          ois.refs["server #1"].color = "#ff0000";
+          ois.refs["plane #1"].color = "#ff0000";
+          ois.refs["line #1"].color = "#ff0000";
+
+          ois.refs["text #1"].text = "DOWN";
+          ois.refs["text #1"].color = "#ff0000";
+        }
+        // console.log(ois.makeConfig());
+      }, 1000);
+    },
+    true
+  );
+});
 </script>
 
 <style>
@@ -14,4 +46,3 @@ console.log(RIS);
   background: var(--bg);
 }
 </style>
-
